34a184e717664d6e170852dced222d95a8a2d5db
bufferOverflow06 <sujanjacharya123@gmail.com>
2025-04-12 20:44:05 +0530

    Added SpringBootWeb1/     - MVC flow

---
+++ SpringFramework/SpringBoot/SpringBoot Web/SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
@@ -0,0 +1,36 @@
+package com.example.SpringBootWeb1;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String home(){
+        System.out.println("home method called");
+        return "index";
+    }
+
+    @RequestMapping("add")
+    public ModelAndView add(@RequestParam("num1") int num1,
+                            @RequestParam("num2") int num2,
+                            ModelAndView mv){
+//        int num1 = Integer.parseInt(req.getParameter("num1"));
+//        int num2 = Integer.parseInt(req.getParameter("num2"));
+        int result = num1 + num2;
+
+//        session.setAttribute("result", result);
+//        model.addAttribute("result", result);
+        mv.addObject("result", result);
+        mv.setViewName("result");
+
+        System.out.println(result);
+        return mv;
+    }
+}
---
+++ SpringFramework/SpringBoot/SpringBoot Web/SpringBootWeb1/src/main/webapp/views/result.jsp
@@ -0,0 +1,11 @@
+<%@page language="java" %>
+
+<html>
+    <head>
+    </head>
+    <body>
+        <!-- <h2>Result is: <%= session.getAttribute("result") %></h2> --!>
+        <!-- Using JSTL --!>
+        <h2>Result is: ${result} </h2>
+    </body>
+</html>
