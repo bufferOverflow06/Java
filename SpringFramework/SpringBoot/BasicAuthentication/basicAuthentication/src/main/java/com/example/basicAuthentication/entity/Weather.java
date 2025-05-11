b2db8b1219e0114653c4fccd23a830a368a2823e
Sujan J Acharya <sujanjacharya123@gmail.com>
2025-05-11 11:46:09 +0530

    Added /entity/     - User and Weather

---
+++ SpringFramework/SpringBoot/BasicAuthentication/basicAuthentication/src/main/java/com/example/basicAuthentication/entity/Weather.java
@@ -0,0 +1,48 @@
+package com.example.basicAuthentication.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "weather")
+public class Weather{
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String city;
+    private String forecast;
+
+    public Weather() {
+    }
+
+    public Weather(String city, String forecast) {
+        this.city = city;
+        this.forecast = forecast;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getForecast() {
+        return forecast;
+    }
+
+    public void setForecast(String forecast) {
+        this.forecast = forecast;
+    }
+}
