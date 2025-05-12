b81d1cea62d1462048a4fb5cf0ec2378cdae043f
Sujan J Acharya <sujanjacharya123@gmail.com>
2025-05-12 18:55:28 +0530

    Added AdminUserInitializer.java     - adding admin credentials at runtime     - checking for user credentials from database

---
+++ SpringFramework/SpringBoot/BasicAuthentication/basicAuthentication/src/main/java/com/example/basicAuthentication/service/AdminUserInitializer.java
@@ -0,0 +1,28 @@
+package com.example.basicAuthentication.service;
+
+import com.example.basicAuthentication.entity.Users;
+import com.example.basicAuthentication.repository.UserDetailsRepository;
+import org.apache.catalina.User;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminUserInitializer {
+
+    @Bean
+    public CommandLineRunner createAdminUser(UserDetailsRepository userDetailsRepository, PasswordEncoder passwordEncoder){
+        return args -> {
+            if(userDetailsRepository.findByUsername("admin").isEmpty()){
+                Users user = new Users();
+                user.setName("admin");
+                user.setPassword(passwordEncoder.encode("admin123"));
+                user.setRole("ADMIN");
+
+                userDetailsRepository.save(user);
+                System.out.println("Default admin user created");
+            }
+        };
+    }
+}
