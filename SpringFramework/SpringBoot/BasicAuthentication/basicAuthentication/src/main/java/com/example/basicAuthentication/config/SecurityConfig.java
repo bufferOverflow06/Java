f5c37f95a4d84b42af27dcc10c02cd01419ec115
Sujan J Acharya <sujanjacharya123@gmail.com>
2025-05-12 18:48:24 +0530

    Adding Custom Authenticatication manager     - DaoAuthenticationManager     - along with PasswordEncoder

--- SpringFramework/SpringBoot/BasicAuthentication/basicAuthentication/src/main/java/com/example/basicAuthentication/config/SecurityConfig.java
+++ SpringFramework/SpringBoot/BasicAuthentication/basicAuthentication/src/main/java/com/example/basicAuthentication/config/SecurityConfig.java
@@ -1,11 +1,19 @@
 package com.example.basicAuthentication.config;
 
+import com.example.basicAuthentication.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,9 +22,30 @@ public class SecurityConfig{
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated()) // for all requests
+        http.authorizeHttpRequests(auth ->
+            auth.requestMatchers("/h2-console").permitAll() // using this we can permit certain patterns
+            .anyRequest().authenticated()) // for all requests
             .httpBasic(Customizer.withDefaults()); // enabling basic authenticaion
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new CustomUserDetailsService();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+
+        return new ProviderManager(daoAuthenticationProvider);
     }
 
 }
