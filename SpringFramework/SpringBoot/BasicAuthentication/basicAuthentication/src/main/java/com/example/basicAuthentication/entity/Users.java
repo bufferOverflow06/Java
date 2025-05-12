252fcfcdd5158949382a581a8b9873f40173d8f9
Sujan J Acharya <sujanjacharya123@gmail.com>
2025-05-12 18:49:16 +0530

    As AuthenticationManager(DaoAuthenticationManager) expects UserDetails type     - We are implementing all the methods in UserDetails interface in       User class

--- SpringFramework/SpringBoot/BasicAuthentication/basicAuthentication/src/main/java/com/example/basicAuthentication/entity/Users.java
+++ SpringFramework/SpringBoot/BasicAuthentication/basicAuthentication/src/main/java/com/example/basicAuthentication/entity/Users.java
@@ -4,9 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
-public class Users{
+public class Users implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,9 +34,41 @@ public class Users{
     public void setName(String username) {
         this.username = username;
     }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
     public String getPassword() {
         return password;
     }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
     public void setPassword(String password) {
         this.password = password;
     }
