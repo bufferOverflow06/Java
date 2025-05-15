18bead8b7236aa62720b6ee552c4e0047e74b957
Sujan J Acharya <sujanjacharya123@gmail.com>
2025-05-15 19:38:00 +0530

    Added /configs/JwtAuthenticationFilter.java

--- SpringFramework/SpringBoot/JwtAuthentication/src/main/java/com/example/JwtAuthentication/configs/JwtAuthenticationFilter.java
+++ SpringFramework/SpringBoot/JwtAuthentication/src/main/java/com/example/JwtAuthentication/configs/JwtAuthenticationFilter.java
@@ -1,4 +1,85 @@
 package com.example.JwtAuthentication.configs;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import com.example.JwtAuthentication.services.JwtService;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final HandlerExceptionResolver handlerExceptionResolver;
+
+    private final JwtService jwtService;
+
+    private final UserDetailsService userDetailsService;
+
+
+    public JwtAuthenticationFilter(HandlerExceptionResolver handlerExceptionResolver,
+                                   JwtService jwtService,
+                                   UserDetailsService userDetailsService
+    ) {
+        this.handlerExceptionResolver = handlerExceptionResolver;
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        try{
+            final String jwt = authHeader.substring(7);
+            final String userEmail = jwtService.extractUsername(jwt);
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            if(userEmail != null && authentication == null){
+                UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+
+                if(jwtService.isTokenValid(jwt, userDetails)){
+                    // Spring security's authentication object (remember from diagram)
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                    );
+                    // creating an authentication object that spring security understands
+
+                    // adding extra details to authtoken
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    // Set authentication in SecurityContext
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                    // Future filters and controllers can access the user using SecurityContextHolder
+                }
+            }
+
+            // pass control to next filter or eventually to controller
+            filterChain.doFilter(request, response);
+
+        }
+        catch(Exception exception){
+            handlerExceptionResolver.resolveException(request, response, null, exception);
+        }
+    }
 }
