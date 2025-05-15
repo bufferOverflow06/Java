3f25c855b1d932d5ad83b7440549dc9499b7606e
Sujan J Acharya <sujanjacharya123@gmail.com>
2025-05-15 19:36:01 +0530

    Added /services/AuthenticationService

---
+++ SpringFramework/SpringBoot/JwtAuthentication/src/main/java/com/example/JwtAuthentication/services/AuthenticationService.java
@@ -0,0 +1,46 @@
+import org.apache.catalina.User;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.JwtAuthentication.dtos.LoginUserDto;
+import com.example.JwtAuthentication.dtos.RegisterUserDto;
+import com.example.JwtAuthentication.entities.Users;
+import com.example.JwtAuthentication.repository.UserRepository;
+
+@Service
+public class AuthenticationService{
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationService(UserRepository userRepository, PasswordEncoder passwordEncoder,
+            AuthenticationManager authenticationManager) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+    }
+
+    public Users signup(RegisterUserDto input){
+        Users user = new Users();
+        user.setFullName(input.getFullname());
+        user.setEmail(input.getEmail());
+        user.setPassword(passwordEncoder.encode(input.getPassword()));
+
+        return userRepository.save(user);
+    }
+
+    public Users authenticate(LoginUserDto input){
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                input.getEmail(),
+                input.getPassword()
+            )
+        );
+        return userRepository.findByEmail(input.getEmail())
+            .orElseThrow();
+    }
+}
