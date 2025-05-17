250122c4d3af774d038ef44a4e16b3f0409cd7af
bufferOverflow06 <sujanjacharya123@gmail.com>
2025-05-17 15:23:17 +0530

    Added Category entity

---
+++ SpringFramework/SpringBoot/ProductManagement/src/main/java/com/example/ProductManagement/OneToMany/model/Category.java
@@ -0,0 +1,60 @@
+package com.example.ProductManagement.OneToMany.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer categoryId;
+
+    private String categoryName;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Product> productList;
+
+    public Category() {
+    }
+
+    public Category(Integer categoryId, String categoryName, List<Product> productList) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        this.productList = productList;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "categoryId=" + categoryId +
+                ", categoryName='" + categoryName + '\'' +
+                ", productList=" + productList +
+                '}';
+    }
+}
