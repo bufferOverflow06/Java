e2d956a8aec03baa25d22ec245a0d57862e75ca8
bufferOverflow06 <sujanjacharya123@gmail.com>
2025-06-16 22:23:22 +0530

    Removed bus_booking     - making it a new repository

--- SpringFramework/SpringBoot/bus_booking/src/main/java/com/example/bus_booking/repository/AgentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bus_booking.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.bus_booking.entity.Agent;
-
-public interface AgentRepository extends JpaRepository<Agent, Long>{
-
-}
